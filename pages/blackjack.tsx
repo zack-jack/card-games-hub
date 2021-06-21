72bb8e47c503ced38d2f2b402bb512133ba22e02
zack-jack <zjackson@redventures.com>
2021-06-21 12:30:54 -0400

    add tailwindcss

--- pages/blackjack.tsx
+++ pages/blackjack.tsx
@@ -1,15 +1,14 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Blackjack() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Blackjack | Card Games Hub</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           Welcome to Blackjack!
         </h1>
       </main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Card Games Hub</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           Welcome to Card Games Hub!
         </h1>
       </main>
